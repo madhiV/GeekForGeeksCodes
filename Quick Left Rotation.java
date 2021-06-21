f9d5678c41437a625ea367a325b2a2ddbb963ee8
Madhivadhanan B R <72497080+madhiV@users.noreply.github.com>
2021-06-21 20:25:48 +0530

    Update Quick Left Rotation.java

--- Quick Left Rotation.java
+++ Quick Left Rotation.java
@@ -3,16 +3,16 @@ class Solution
     static void leftRotate(long arr[], int k,int n)
     {
         k=k%n;
-        Queue<Long> q=new LinkedList<>();
-        for(int i=0;i<k;i++){
-            q.add(arr[i]);
-        }
-        int i=0;
-        for(int j=k;j<n;j++){
-            arr[i]=arr[j];
-            i++;
-        }
-        while(!q.isEmpty()){
-            arr[i]=q.poll();
-            i++;
+        reverse(arr,0,n-1);
+        reverse(arr,0,n-k-1);
+        reverse(arr,n-k,n-1);
+    }
+    static void reverse(long[] arr,int l,int r){
+        long temp;
+        while(l<r){
+            temp=arr[l];
+            arr[l]=arr[r];
+            arr[r]=temp;
+            l++;
+            r--;
         }
